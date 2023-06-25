cf9ad74c5c7ea8566619b15ad31bee00bbef243d
Evandro Santos <evandro.santos@gmail.com>
2023-06-24 23:22:16 -0300

    add validations to company invite user model

--- app/models/company_user_invitation.rb
+++ app/models/company_user_invitation.rb
@@ -1,3 +1,13 @@
+# typed: strict
+require 'sorbet-runtime'
+
 class CompanyUserInvitation < ApplicationRecord
+  extend T::Sig
+
   belongs_to :company
+  has_secure_token :token
+
+  validates :user_email, presence: true, length: { maximum: 100 }, confirmation: true, uniqueness: { scope: :company }, format: { with: RegexUtil::EMAIL_REGEX }
+  validates :user_email_confirmation, presence: true, if: Proc.new { |u| u.user_email_changed? }
+  validates :token, uniqueness: true, allow_nil: true
 end
